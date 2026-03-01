cfd57a5677c18045ebb3c09d36bc00055503ba8e
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2026-03-01 12:52:35 -0800

    Init commit

---
+++ index.js
@@ -0,0 +1,101 @@
+require('dotenv').config()
+
+// Getting all tasks that have morning, afternoon, and evenining tags
+async function getData(){
+    console.log(process.env.TEST_DATA_SOURCE)
+    const response = await fetch(`https://api.notion.com/v1/data_sources/${process.env.TEST_DATA_SOURCE}/query`, {
+    method: 'POST',
+    headers: {
+        'Authorization': `${process.env.NOTION_TOKEN}`,
+        'Notion-Version': '2025-09-03',
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        filter: {
+            "and": [
+                {
+                    property: "Checkbox",
+                    checkbox: { equals: true},   
+                },
+                {
+                    "or": [
+                        {
+                            property: 'Tags',
+                            multi_select: { contains: "Morning" },
+                        },
+                        {
+                            property: 'Tags',
+                            multi_select: { contains: "Afternoon"},
+                        },
+                        {
+                            property: 'Tags',
+                            multi_select: { contains: "Evening"},
+                        }
+                    ]
+                }
+            ]
+        }
+    })
+    })
+    const data = await response.json()
+    console.log("Retrieved Checked Tasks")
+    removeChecks(data)
+}
+
+getData()
+
+// Uncheck all checkboxes for filtered tasks
+async function removeChecks(tasks) {
+    console.log(`Removing ${tasks.results.length} from tasks`)
+    for (let i = 0; i < tasks.results.length; i++) {
+        console.log(`Starting to uncheck ${tasks.results[i].properties.Name.title[0].plain_text}, #${i}`)
+        const response = await fetch(`https://api.notion.com/v1/pages/${tasks.results[i].id}`, {
+            method: 'PATCH',
+            headers: {
+                'Authorization': `Bearer ${process.env.NOTION_TOKEN}`,
+                'Notion-Version': '2025-09-03',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                properties: {
+                    Checkbox: {
+                        checkbox: false,
+                    } 
+                }
+            })
+        })
+        const data = await response.json()
+        console.log(`Unchecked ${tasks.results[i].properties.Name.title[0].plain_text}`)
+    }
+    hasMore(tasks)
+}
+
+// If tasks number exceeds 100, request next batch
+async function hasMore(data){
+    if (data.has_more) {
+        console.log("Has more indeed lmao")
+        let current_data = data
+        let has_more = data.has_more
+        while (has_more) {
+            const response = await fetch(`https://api.notion.com/v1/data_sources/${process.env.TEST_DATA_SOURCE}/query`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `${process.env.NOTION_TOKEN}`,
+                    'Notion-Version': '2025-09-03',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    start_cursor: current_data.next_cursor,
+                })
+            })
+            const temp = await response.json()
+            console.log("This block did run")
+            removeChecks(temp)
+            if (temp.has_more) {
+                current_data = temp
+            } else {
+                has_more = false
+            }
+        } 
+    }
+}
