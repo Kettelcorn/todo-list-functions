f16ab40a55afe64dfb8a71ae8af56c7d18f64a9e
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2026-03-06 16:16:30 -0800

    Able to trigger a run in the stage database locally

--- src/environments/stage.js
+++ src/environments/stage.js
@@ -2,8 +2,27 @@ const app = require("../app.js")
 require('dotenv').config()
 
 async function execute() {
-    const data_source = await app.getDataSourceId(process.env.STAGE_DATA_URL);
-    app.main(data_source)
+    if (process.env.NODE_ENV == 'local') {
+        let data;
+        try {
+            const result = await fetch(`https://api.render.com/v1/services/${process.env.RENDER_SERVICE_ID}/jobs`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${process.env.RENDER_TOKEN}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    startCommand: 'npm run stage' 
+                })
+            })
+            data = await result.json();
+        } catch (error) {
+            console.error("Failed to trigger one-off job: ", error);
+        }
+    } else {
+        const data_source = await app.getDataSourceId(process.env.STAGE_DATA_URL);
+        app.main(data_source)
+    }
 }
 
 execute();
