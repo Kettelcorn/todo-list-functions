17742c3e712f594e7481d2882a75f5a35bd1c816
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2026-03-26 17:26:58 -0700

    Updated tests

--- tests/test.js
+++ tests/test.js
@@ -35,29 +35,32 @@ test('main test', async (t) => {
         if (data_source == null) {
             data_source = await requests.getDataSourceId(process.env.TEST_DATA_URL);
         }
-        const filteredTasks = await requests.getTasks(data_source, recurringFilter);
-        await randomUpdate(filteredTasks.splice(0, 50));
         const originalTasks = await requests.getTasks(data_source, recurringFilter);
-        await app.updateRecurring(data_source);
-        const updatedTasks = await requests.getTasks(data_source, recurringFilter);
-        for (let i = 0; i < updatedTasks.length; i++) {
-            if (originalTasks[i].properties.Number.number !== null && originalTasks[i].properties.Number.number !== 0) {
-                assert.strictEqual(
-                    updatedTasks[i].properties.Number.number,
-                    originalTasks[i].properties.Number.number - 1
-                );
-                if (originalTasks[i].properties.Number.number == 1) {
-                    assert.ok(!updatedTasks[i].properties.Checkbox.checkbox);
-                }
-            } else {
-                if (originalTasks[i].properties.Checkbox.checkbox === true) {
+        const results = await app.updateRecurring(data_source);
+        if (results) {
+            const updatedTasks = await requests.getTasks(data_source, recurringFilter);
+            for (let i = 0; i < updatedTasks.length; i++) {
+                if (
+                    originalTasks[i].properties.Number.number !== null &&
+                    originalTasks[i].properties.Number.number !== 0
+                ) {
                     assert.strictEqual(
                         updatedTasks[i].properties.Number.number,
-                        requests.getDayCount(updatedTasks[i]) - 1
+                        originalTasks[i].properties.Number.number - 1
                     );
-                    assert.ok(updatedTasks[i].properties.Checkbox.checkbox);
+                    if (originalTasks[i].properties.Number.number == 1) {
+                        assert.ok(!updatedTasks[i].properties.Checkbox.checkbox);
+                    }
                 } else {
-                    assert.ok(!updatedTasks[i].properties.Checkbox.checkbox);
+                    if (originalTasks[i].properties.Checkbox.checkbox === true) {
+                        assert.strictEqual(
+                            updatedTasks[i].properties.Number.number,
+                            requests.getDayCount(updatedTasks[i]) - 1
+                        );
+                        assert.ok(updatedTasks[i].properties.Checkbox.checkbox);
+                    } else {
+                        assert.ok(!updatedTasks[i].properties.Checkbox.checkbox);
+                    }
                 }
             }
         }
@@ -105,62 +108,6 @@ test('main test', async (t) => {
         }
     });
 });
-
-// Randomly assigns number and check values to weekly tasks to simulate a random day
-async function randomUpdate(tasks) {
-    let results = [];
-    console.log(`Randomly updating ${tasks.length} tasks...`);
-    for (let i = 0; i < tasks.length; i++) {
-        let randomDays = getRandomInt(0, requests.getDayCount(tasks[i])) - 1;
-        let random;
-        if (randomDays > 0) {
-            random = true;
-        } else {
-            random = getRandomInt(0, 1) === 1;
-        }
-        if (randomDays === -1) {
-            randomDays = null;
-        }
-        try {
-            const response = await fetch(`https://api.notion.com/v1/pages/${tasks[i].id}`, {
-                method: 'PATCH',
-                headers: {
-                    Authorization: `Bearer ${process.env.NOTION_TOKEN}`,
-                    'Notion-Version': '2025-09-03',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    properties: {
-                        Checkbox: {
-                            checkbox: random
-                        },
-                        Number: {
-                            number: randomDays
-                        }
-                    }
-                })
-            });
-            if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
-            }
-            const data = await response.json();
-            results.push(data);
-            console.log(
-                `Assigned ${random} and ${randomDays} to ${tasks[i].properties.Name.title[0].plain_text} (${i + 1})`
-            );
-        } catch (error) {
-            console.error(error);
-        }
-    }
-    return results;
-}
-
-// Generate random int
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 // Checks to see if only daily tasks are only unchecked and not other tasks
 function allUnchecked(tasks) {
